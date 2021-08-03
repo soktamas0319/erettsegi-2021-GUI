6595a82e99bcb21cec0e222e26c1cbc0704d2d8f
Sok Tamás <soktamas@yahoo.com>
2021-08-03 09:07:19 +0200

    Alap ablak netbeansben.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private void handleButtonAction (ActionEvent event) {
+        System.out.println("You clicked me!");
+        //label.setText("Hello World!");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
